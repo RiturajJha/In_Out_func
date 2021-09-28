ea450c78c94aa6906f08e4a6593ebe53ecf4cd48
Ritu Raj Jha <85738906+Ritxam@users.noreply.github.com>
2021-09-28 11:43:01 +0530

    Add files via upload

---
+++ InandOut.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{ 
+ cout<<endl;
+
+ //declaring a variable to store intger value
+ int variable1;
+
+ cout<<"Enter the number =";
+ //storing the integer value
+ cin>>variable1;
+ cout<<endl;
+
+ //declaring another variable to store a string
+ string variable2;
+
+ cout<<"Enter your string =";
+ //storing the string value
+ cin>>variable2;
+ cout<<endl;
+
+ //printing the overall result
+ cout<<variable2<<" is "<<variable1<<" day of the week"<<endl;
+ cout<<endl;
+
+ return 0;
+}
---
+++ func.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+//Declaring function
+int DemoConsoleOutput();
+
+int main()
+{
+ //calling function 
+  DemoConsoleOutput();
+  return 0;
+}
+
+//function definition
+int DemoConsoleOutput()
+{
+ cout<<endl;
+ cout<<"This is a string literal"<<endl;
+ cout<<"Printing number 8"<<endl;
+ cout<<"Performing division 8/4 ="<<8/4<<endl;
+ cout<<"Pi when approximated is 22/7 ="<<22/7<<endl;
+ cout<<"Pi is 22/7 ="<<22.0/7<<endl;
+ cout<<endl;
+
+ return 0;
+} 
+
+
+
